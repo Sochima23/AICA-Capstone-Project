a564a3cff70ac4259dae1886adffe336ce100a6d
Sochima Madueke <130303083+Sochima23@users.noreply.github.com>
2026-06-24 13:02:07 +0100

    Update validate.py

--- etl/validate.py
+++ etl/validate.py
@@ -1,1 +1,21 @@
+def validate_data(df):
 
+    required_columns = [
+        "time",
+        "temperature_2m",
+        "relative_humidity_2m",
+        "date"
+    ]
+
+    missing = [
+        col
+        for col in required_columns
+        if col not in df.columns
+    ]
+
+    if missing:
+        raise ValueError(
+            f"Missing columns: {missing}"
+        )
+
+    return True
